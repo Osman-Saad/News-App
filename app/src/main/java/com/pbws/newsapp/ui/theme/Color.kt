9e75c36138feb51975efe532dfa905e7bd3f5cdc
Osman Saad Osman <osman.saad7121@gmail.com>
2024-08-05 15:26:32 -0700

    Master Branch

---
+++ app/src/main/java/com/pbws/newsapp/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val primaryLight = Color(0xFFFF9900)
+val primaryVariantLight = Color(0xE2FF9900) // 0xE2 should represent alpha, which is 226/255
+val secondaryLight = Color(0xFFFF0000)
+val backgroundLight = Color(0xFFF8F8FA)
+val surfaceLight = Color(0xFFFFFFFF)
+val errorLight = Color(0xFFFF0000)
+val onPrimaryLight = Color(0xFF000000)
+val onSecondaryLight = Color(0xFF000000)
+val onBackgroundLight = Color(0xFF000000)
+val onSurfaceLight = Color(0xFFC1C1C1)
+val onErrorLight = Color(0xFFFF0000)
+val textColorSearchTextLight = Color(0xFF000000)
+
+// Dark Theme Colors
+val primaryDark = Color(0xFFFF9900)
+val primaryVariantDark = Color(0xE2FF9900) // 0xE2 should represent alpha, which is 226/255
+val secondaryDark = Color(0xFFFF0000)
+val backgroundDark = Color(0xFF1C2128)
+val surfaceDark = Color(0xFF373839)
+val errorDark = Color(0xFFFF0000)
+val onPrimaryDark = Color(0xFFFFFFFF)
+val onSecondaryDark = Color(0xFF000000)
+val onBackgroundDark = Color(0xFFFFFFFF)
+val onSurfaceDark = Color(0xFF8D8D8D)
+val onErrorDark = Color(0xFFFF0000)
+val textColorSearchTexDark = Color(0xFF000000)
+
+
+// -----
+val OrangeColor = Color(0xFFFF9900)
---
+++ app/src/main/java/com/pbws/newsapp/ui/theme/Theme.kt
@@ -0,0 +1,74 @@
+package com.pbws.newsapp.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import backgroundDark
+import backgroundLight
+import errorDark
+import errorLight
+import onBackgroundDark
+import onBackgroundLight
+import onErrorDark
+import onErrorLight
+import onPrimaryDark
+import onPrimaryLight
+import onSurfaceDark
+import onSurfaceLight
+import primaryDark
+import primaryLight
+import surfaceDark
+import surfaceLight
+import textColorSearchTexDark
+import textColorSearchTextLight
+
+private val DarkColorScheme = darkColorScheme(
+    primary = primaryDark,
+    background = backgroundDark,
+    onTertiary = textColorSearchTexDark,
+    surface = surfaceDark,
+    error = errorDark,
+    onBackground = onBackgroundDark,
+    onSurface = onSurfaceDark,
+    onError = onErrorDark,
+    onPrimary = onPrimaryDark,
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = primaryLight,
+    background = backgroundLight,
+    surface = surfaceLight,
+    error = errorLight,
+    onTertiary = textColorSearchTextLight,
+    onBackground = onBackgroundLight,
+    onSurface = onSurfaceLight,
+    onError = onErrorLight,
+    onPrimary = onPrimaryLight,
+)
+
+
+@Composable
+fun NewsAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
+
